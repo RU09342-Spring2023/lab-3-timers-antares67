f5d29a0be4b4cf176e531a7dddb183713525dd2b
antares67 <rasath87@students.rowan.edu>
2023-04-24 12:52:23 -0400

    update

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -9,55 +9,53 @@
 
 #include <msp430.h>
 
-char speedTracker = 0;
-char offset = 0;
+unsigned int speedTracker = 0;
+unsigned int offset = 0;
 
 void gpioInit();
 void timerInit();
 
-void main(){
+void main()
+{
 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
 
     gpioInit();
     timerInit();
 
-
     // Disable the GPIO power-on default high-impedance mode
     // to activate previously configured port settings
-    PM5CTL0 &= ~LOCKLPM5;
+   PM5CTL0 &= ~LOCKLPM5;
 
-    __bis_SR_register(GIE); //LPM3_bits |
-
+    __bis_SR_register(LPM3_bits | GIE);
 }
 
-
-void gpioInit(){
+void gpioInit()
+{
     // @TODO Initialize the Red or Green LED
-        P6OUT &= ~BIT6;                         // Clear P6.6 output latch for a defined power-on state
-        P6DIR |= BIT6;                          // Set P6.6 to output direction
+    P6OUT &= ~BIT6;      // Clear P6.6 output latch for a defined power-on state
+    P6DIR |= BIT6;                          // Set P6.6 to output direction
+
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+    P1DIR |= BIT0;
 
     // @TODO Initialize Button 2.3
-        P4OUT |= BIT1;                          // Configure P2.3 as pulled-up
-        P4REN |= BIT1;                          // P2.3 pull-up register enable
-        P4IES &= ~BIT1;                         // P2.3 Low --> High edge
-        P4IE |= BIT1;                           // P2.3 interrupt enabled
-        P4IFG &= 0;
-
-
+    P4OUT |= BIT1;                          // Configure P2.3 as pulled-up
+    P4REN |= BIT1;                          // P2.3 pull-up register enable
+    P4IES &= ~BIT1;                         // P2.3 Low --> High edge
+    P4IE |= BIT1;                           // P2.3 interrupt enabled
+    P4IFG &= 0;
 
 }
 
-void timerInit(){
+void timerInit()
+{
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
-        TB1CCTL0 = CCIE;                          // TBCCR0 interrupt enabled
-        TB1CCR0 = 50000;
-        TB1CTL = TBSSEL_1 | MC_2;                 // ACLK, continuous mode
-
-
+    TB1CCTL0 = CCIE;                          // TBCCR0 interrupt enabled
+    TB1CCR0 = 5000;
+    TB1CTL = TBSSEL_1 | MC_2;                 // ACLK, continuous mode
 
 }
-
 
 /*
  * INTERRUPT ROUTINES
@@ -68,28 +66,27 @@ void timerInit(){
 __interrupt void Port_4(void)
 {
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
-    P4IFG &= 0;
+    P4IFG &= ~BIT1;
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
 
-    switch(speedTracker){
-        case 0:
-            speedTracker = 1;
-            offset = 25000;
+    switch (speedTracker)
+    {
+    case 0:
+        speedTracker = 1;
+        offset = 5000;
         break;
-        case 0x01:
-            speedTracker = 2;
-            offset = 5000;
+    case 1:
+        speedTracker = 2;
+        offset = 2500;
         break;
-        case 0x02:
-            speedTracker = 0;
-            offset = 50000;
+    case 2:
+        speedTracker = 0;
+        offset = 1250;
         break;
 
     }
 
-
 }
-
 
 // Timer B1 interrupt service routine
 #pragma vector = TIMER1_B0_VECTOR
@@ -100,5 +97,4 @@ __interrupt void Timer1_B0_ISR(void)
     TB1CCR0 += offset;
 
 }
-
 
--- 3. Design Challenge Button Based Delay/Part3.c
+++ 3. Design Challenge Button Based Delay/Part3.c
@@ -8,7 +8,94 @@
  */
 
 
+#include <msp430.h>
+    unsigned int delay_count = 8000;
+
+    void gpioInit();
+    void timerInit();
+
+    void main(){
+
+        WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+
+        gpioInit();
+        timerInit();
+
+        PM5CTL0 &= ~LOCKLPM5;
 
 
 
 
+        __bis_SR_register(LPM3_bits | GIE);
+
+        while(1){
+            P1OUT ^= BIT0;
+
+        }
+
+    }
+
+
+    void gpioInit(){
+
+        P1OUT &= ~BIT0; //RED LED
+        P1DIR |= BIT0;  //Set as output
+
+        ///BUTTONS
+        //LEFT BUTTON - reset button
+        P4OUT |= BIT1;                          // Configure P4.1 as pulled-up
+        P4REN |= BIT1;                          // P2.3 pull-up register enable
+        P4IES &= ~BIT1;                         // P2.3 Low --> High edge
+        P4IE |= BIT1;                           // P2.3 interrupt enabled
+        P4IFG &= 0;
+
+        //RIGHT BUTTON- time capture button
+        P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+        P2REN |= BIT3;                          // P2.3 pull-up register enable
+        P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+        P2IE |= BIT3;                           // P2.3 interrupt enabled
+        P2IFG &= 0;
+
+    }
+
+    void timerInit(){
+
+        TB1CCTL0 = CCIE;                          // TBCCR0 interrupt enabled
+        TB1CCR0 = delay_count;
+        TB1CTL = TBSSEL_1 | MC_1;                 // ACLK, continuous mode
+
+    }
+
+
+    // Port 2 interrupt service routine
+    #pragma vector=PORT2_VECTOR
+    __interrupt void Port_2(void)
+    {
+        P2IFG &= ~BIT3;             // Clear P2.3 IFG
+        P1OUT &= ~BIT0;             //turn off LED while capturing time
+
+        while(!P2IN){
+            delay_count++;
+        }
+    }
+
+
+    // Port 4 interrupt service routine
+    #pragma vector=PORT4_VECTOR
+    __interrupt void Port_4(void)
+    {
+        P4IFG &= ~BIT1;                         // Clear P4.1 IFG
+        P1OUT ^= BIT0;
+
+    }
+
+
+    // Timer B1 interrupt service routine
+    #pragma vector = TIMER1_B0_VECTOR
+    __interrupt void Timer1_B0_ISR(void)
+    {
+        // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+        P1OUT ^= BIT0;
+
+
+    }
